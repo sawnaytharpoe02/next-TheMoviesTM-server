38ff6780472aed2cf26303fb91b16163b04374d1
Saw Nay Thar Poe <100279951+sawnaytharpoe02@users.noreply.github.com>
2023-06-06 14:17:10 +0630

    remove helmet

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const auth = require('json-server-auth');
 const app = jsonServer.create();
 const router = jsonServer.router('db.json');
 const cors = require('cors');
-const helmet = require('helmet');
 
 // /!\ Bind the router db to the app
 app.db = router.db;
@@ -15,8 +14,6 @@ const rules = auth.rewriter({
 });
 
 app.use(cors());
-app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
 // You must apply the middlewares in the following order
 app.use(rules);
 // You must apply the auth middleware before the router
