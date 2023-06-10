775a3be27765a6a644938c662449b22b1be610fd
Saw Nay Thar Poe <100279951+sawnaytharpoe02@users.noreply.github.com>
2023-06-10 10:13:22 +0630

    initial state

--- index.js
+++ index.js
@@ -1,32 +1,21 @@
-import { create, router as _router } from 'json-server';
-import auth, { rewriter } from 'json-server-auth';
-import cors from 'cors';
+const jsonServer = require('json-server');
+const auth = require('json-server-auth');
 
-const app = create();
-const router = _router('db.json');
-
+const app = jsonServer.create();
+const router = jsonServer.router('db.json');
+const cors = require('cors');
 // /!\ Bind the router db to the app
 app.db = router.db;
-const rules = rewriter({
+const rules = auth.rewriter({
   // Permission rules
   users: 660,
+  movie_favorite: 660,
 });
 
 app.use(cors());
-app.options('*', cors()); // Enable preflight requests
+// You must apply the middlewares in the following order
 app.use(rules);
+// You must apply the auth middleware before the router
 app.use(auth);
 app.use(router);
-
-// Custom middleware to set the Access-Control-Allow-Origin header
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PUT, PATCH, DELETE'
-  );
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
-
 app.listen(3000);
